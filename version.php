96b48e0be7ea10942faaf4cbfb35810a9911e6ae
SUNIE <sunieadm@moodle-test.grenet.fr>
2023-11-02 10:50:04 +0100

    update version

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2020010700;
+$plugin->version = 2030010700;
 $plugin->component = 'local_fullscreen';
-$plugin->release = '3.0.0 (2020-01-07)';
+$plugin->release = '4.0.0 (2023-02-11)';
 $plugin->requires = 2019111200;
 $plugin->maturity = MATURITY_STABLE;
